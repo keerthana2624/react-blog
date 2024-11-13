2723000e9c19fc8b2eeeef9d1bc5ac105f0ded4e
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-13 18:53:47 +0530

    implimented the stylings for Bloglist

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -1,16 +1,19 @@
 
-import Header from './components/Header'
-import './App.css'
+import Header from './components/Header';
+import BlogList from './components/BlogList/BlogList';
+import { posts } from './data/posts';
+import './App.css';
+
 function App() {
   return (
     <div className="app">
       <Header />
-      <main>
-        <h2>Welcome to my blog!</h2>
-        <p>This is my first React component.</p>
+      <main className="main-content">
+        <BlogList posts={posts} />
       </main>
     </div>
   );
 }
 
 export default App;
+
