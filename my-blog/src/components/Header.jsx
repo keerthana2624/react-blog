6059ff1230c0b1c11b634d78bf6bcfd627861765
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-13 18:14:58 +0530

    writtern the project structure and what i learned from this project

---
+++ my-blog/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+import './Header.css';
+function Header() {
+    return (
+      <header className="blog-header">
+        <h1>My Awesome Blog</h1>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
