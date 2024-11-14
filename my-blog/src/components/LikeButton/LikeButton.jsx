b4c7013e8b53660c51044874217e598831648bbc
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-14 12:00:59 +0530

    implimented the loadingstate component

--- my-blog/src/components/LikeButton/LikeButton.jsx
+++ my-blog/src/components/LikeButton/LikeButton.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+// import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types';
-import './LikeButton.css';
+// import './LikeButton.css';
 
 function LikeButton({ initialLikes = 0, onLike }) {
   const [liked, setLiked] = useState(false);
